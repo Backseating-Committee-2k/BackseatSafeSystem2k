56317b55e07396490570cc8772a2c30df5c477fc
Peter Wischer <peter.wischer@lwl.org>
2022-03-07 19:19:47 +0100

    improve the `opcodes` macro
    in the register() you now need a capital letter `R` and a field name `register`:
    `register(R register)`
    `register(T target, S source)`

--- src/opcodes.rs
+++ src/opcodes.rs
@@ -141,14 +141,14 @@ macro_rules! opcodes {
     ( $({
         $identifier:ident,
         $code:literal,
-        register( $( $register_name:ident ),* ) $(, $type:ident )?,
+        register( $( $register_letter:ident $register_name:ident ),* ) $(, $type:ident )?,
         $comment:literal
     },)+ ) => {
         /// ## Opcodes
         /// | Opcode                | Meaning                                   |
         /// |-----------------------|-------------------------------------------|
         $(
-            #[doc = concat!(" | `", stringify!($code), "\u{00a0}", stringify_registers!(($( $register_name ),*) $(, $type)?), "` | ", $comment, " |")]
+            #[doc = concat!(" | `", stringify!($code), "\u{00a0}", stringify_registers!(($( $register_letter ),*) $(, $type)?), "` | ", $comment, " |")]
         )+
         #[derive(Clone, Copy, Debug)]
         pub enum Opcode {
@@ -170,27 +170,27 @@ macro_rules! opcodes {
 }
 
 opcodes!(
-    { MoveRegisterImmediate, 0x0000,  register(r), immediate, "move the value C into register R" },
-    { MoveRegisterAddress, 0x0001,  register(r), address, "move the value at address A into register R" },
-    { MoveTargetSource, 0x0002, register(t, s), "move the contents of register S into register T" },
-    { MoveAddressRegister, 0x0003, register(r), address, "move the contents of register R into memory at address A" },
-    { MoveTargetPointer, 0x0004, register(t, p), "move the contents addressed by the value of register P into register T" },
-    { MovePointerSource, 0x0005, register(p, s), "move the contents of register S into memory at address specified by register P" },
+    { MoveRegisterImmediate, 0x0000, register(R register), immediate, "move the value C into register R" },
+    { MoveRegisterAddress, 0x0001, register(R register), address, "move the value at address A into register R" },
+    { MoveTargetSource, 0x0002, register(T target, S source), "move the contents of register S into register T" },
+    { MoveAddressRegister, 0x0003, register(R register), address, "move the contents of register R into memory at address A" },
+    { MoveTargetPointer, 0x0004, register(T target, P pointer), "move the contents addressed by the value of register P into register T" },
+    { MovePointerSource, 0x0005, register(P pointer, S source), "move the contents of register S into memory at address specified by register P" },
     { HaltAndCatchFire, 0x0006, register(), "halt and catch fire" },
-    { AddTargetLhsRhs, 0x0007, register(t, l, r), "add the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
-    { SubtractTargetLhsRhs, 0x0008, register(t, l, r), "subtract (without carry) the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
-    { SubtractWithCarryTargetLhsRhs, 0x0009, register(t, l, r), "subtract (with carry) the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
-    { MultiplyHighLowLhsRhs, 0x000A, register(h, t, l, r), "multiply the values in registers LL and RR, store the low part of the result in TT, the high part in HH, set zero and carry flags appropriately" },
-    { DivmodTargetModLhsRhs, 0x000B, register(d, m, l, r), "divmod the values in registers LL and RR, store the result in DD and the remainder in MM set zero and divide-by-zero flags appropriately" },
-    { AndTargetLhsRhs, 0x000C, register(t, l, r), "and the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
-    { OrTargetLhsRhs, 0x000D, register(t, l, r), "or the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
-    { XorTargetLhsRhs, 0x000E, register(t, l, r), "xor the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
-    { NotTargetSource, 0x000F, register(t, s), "not the value in register SS, store the result in TT, set zero flag appropriately" },
-    { LeftShiftTargetLhsRhs, 0x0010, register(t, l, r), "left shift the value in register LL by RR bits, store the result in TT, set zero and carry flags appropriately" },
-    { RightShiftTargetLhsRhs, 0x0011, register(t, l, r), "right shift the value in register LL by RR bits, store the result in TT, set zero and carry flags appropriately" },
-    { AddTargetSourceImmediate, 0x0012, register(t, s), immediate, "add the constant CC to the value in register SS and store the result in TT, set zero and carry flags appropriately" },
-    { SubtractTargetSourceImmediate, 0x0013, register(t, s), immediate, "subtract the constant CC from the value in register SS and store the result in TT, set zero and carry flags appropriately" },
-    { CompareTargetLhsRhs, 0x0014, register(t, l, r), "compare the values in registers LL and RR, store the result (Word::MAX, 0, 1) in TT, set zero flag appropriately" },
-    { PushRegister, 0x0015, register(r), "push the value of register RR onto the stack" },
-    { PopRegister, 0x0016, register(r), "pop from the stack and store the value in register RR" },
+    { AddTargetLhsRhs, 0x0007, register(T target, L lhs, R rhs), "add the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
+    { SubtractTargetLhsRhs, 0x0008, register(T target, L lhs, R rhs), "subtract (without carry) the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
+    { SubtractWithCarryTargetLhsRhs, 0x0009, register(T target, L lhs, R rhs), "subtract (with carry) the values in registers LL and RR, store the result in TT, set zero and carry flags appropriately" },
+    { MultiplyHighLowLhsRhs, 0x000A, register(H high, T low, L lhs, R rhs), "multiply the values in registers LL and RR, store the low part of the result in TT, the high part in HH, set zero and carry flags appropriately" },
+    { DivmodTargetModLhsRhs, 0x000B, register(D result, M reminder, L lhs, R rhs), "divmod the values in registers LL and RR, store the result in DD and the remainder in MM set zero and divide-by-zero flags appropriately" },
+    { AndTargetLhsRhs, 0x000C, register(T target, L lhs, R rhs), "and the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
+    { OrTargetLhsRhs, 0x000D, register(T target, L lhs, R rhs), "or the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
+    { XorTargetLhsRhs, 0x000E, register(T target, L lhs, R rhs), "xor the values in registers LL and RR, store the result in TT, set zero flag appropriately" },
+    { NotTargetSource, 0x000F, register(T target, S source), "not the value in register SS, store the result in TT, set zero flag appropriately" },
+    { LeftShiftTargetLhsRhs, 0x0010, register(T target, L lhs, R rhs), "left shift the value in register LL by RR bits, store the result in TT, set zero and carry flags appropriately" },
+    { RightShiftTargetLhsRhs, 0x0011, register(T target, L lhs, R rhs), "right shift the value in register LL by RR bits, store the result in TT, set zero and carry flags appropriately" },
+    { AddTargetSourceImmediate, 0x0012, register(T target, S source), immediate, "add the constant CC to the value in register SS and store the result in TT, set zero and carry flags appropriately" },
+    { SubtractTargetSourceImmediate, 0x0013, register(T target, S source), immediate, "subtract the constant CC from the value in register SS and store the result in TT, set zero and carry flags appropriately" },
+    { CompareTargetLhsRhs, 0x0014, register(T target, L lhs, R rhs), "compare the values in registers LL and RR, store the result (Word::MAX, 0, 1) in TT, set zero flag appropriately" },
+    { PushRegister, 0x0015, register(R register), "push the value of register RR onto the stack" },
+    { PopRegister, 0x0016, register(R register), "pop from the stack and store the value in register RR" },
 );

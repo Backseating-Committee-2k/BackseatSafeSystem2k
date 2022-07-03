e48efd831c9c77d403430dfcf07d107f5e358545
Michael Gerhold <michael.gerhold@web.de>
2022-07-03 16:26:30 +0200

    fixed alignmed of address constant for TERMINAL_CURSOR_MODE

--- src/address_constants.rs
+++ src/address_constants.rs
@@ -2,13 +2,13 @@ use crate::{display, terminal, Address, Size, Word};
 
 pub const TERMINAL_BUFFER_START: Address = 0;
 pub const TERMINAL_BUFFER_SIZE: usize =
-    ((terminal::WIDTH * terminal::HEIGHT + 2) as Address * Word::SIZE as Address) as usize;
+    ((terminal::WIDTH * terminal::HEIGHT) as Address * Word::SIZE as Address) as usize;
 pub const TERMINAL_CURSOR_INDEX: Address = TERMINAL_BUFFER_START + TERMINAL_BUFFER_SIZE as Address;
 pub const TERMINAL_CURSOR_MODE: Address =
-    TERMINAL_BUFFER_START + TERMINAL_BUFFER_SIZE as Address + 1;
+    TERMINAL_BUFFER_START + TERMINAL_BUFFER_SIZE as Address + Word::SIZE as Address;
 pub const FRAMEBUFFER_SIZE: usize = display::WIDTH * display::HEIGHT * 4; // RGBA
 pub const FIRST_FRAMEBUFFER_START: Address =
-    TERMINAL_BUFFER_START + TERMINAL_BUFFER_SIZE as Address;
+    TERMINAL_BUFFER_START + TERMINAL_BUFFER_SIZE as Address + 2 * Word::SIZE as Address /* 2 extra words for Cursor data */;
 pub const SECOND_FRAMEBUFFER_START: Address = FIRST_FRAMEBUFFER_START + FRAMEBUFFER_SIZE as Address;
 pub const STACK_START: Address = SECOND_FRAMEBUFFER_START + FRAMEBUFFER_SIZE as Address;
 pub const STACK_SIZE: usize = 512 * 1024;
